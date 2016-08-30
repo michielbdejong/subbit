7695e8dd32f7b98ca64a094a88240b30c374cbb8
Michiel de Jong <michiel@unhosted.org>
2016-08-30 18:14:14 +0700

    Ran circuits-4-4.txt now, idea for blogpost

--- groups/run.sh
+++ groups/run.sh
@@ -15,8 +15,10 @@ node gen-circuits-n-0.js 4
 node enumerate.js 4 1
 node enumerate.js 4 2
 node enumerate.js 4 3
+node enumerate.js 4 4
+node enumerate.js 4 5
 
 node count.js 1 1
 node count.js 2 1
 node count.js 3 4
-node count.js 4 3
+node count.js 4 5
