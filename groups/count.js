4a7c01a05588d4885677165f5ede46f02a2c014b
Michiel de Jong <michiel@unhosted.org>
2016-08-30 22:44:38 +0700

    More efficient count.js

--- groups/count.js
+++ groups/count.js
@@ -6,7 +6,7 @@ var numValuations = Math.pow(2, numVars);  // for 3 vars, 8 - 000-111
 var numFlags = Math.pow(2, numValuations-1) - 1; // for 3 vars, 2^7-1=127 - 00000001 - 01111111
 
 var maxCircuitSize = parseInt(process.argv[3]);
-var circuits = [];
+var perFlag = {};
 
 function readIn(circuitSize, callback) {
   
@@ -26,16 +26,20 @@ function readIn(circuitSize, callback) {
 
     var foundUnreadableLine = false;
     lineReader.on('line', function (line) {
-      var circuit;
+      var obj;
       try {
-        circuit = JSON.parse(line);
+        obj = JSON.parse(line);
       } catch (e) {
         foundUnreadableLine = true;
       }
-      circuits.push(circuit);
+      for (var wireI=0; wireI<obj.stack.length; wireI++) {
+        if (typeof perFlag[obj.stack[wireI]] === 'undefined') {
+          perFlag[obj.stack[wireI]] = obj.circuit.slice(0, wireI - numVars + 1);
+        }
+      }
     });
     lineReader.on('close', function() {
-      console.log(`Have ${circuits.length} circuits now`);
+      console.log(`Have ${Object.keys(perFlag).length} circuits now`);
       if (foundUnreadableLine) {
         console.error(`Found unreadable line in circuits-${numVars}-${circuitSize}.txt`);
         return;
@@ -51,22 +55,11 @@ function readIn(circuitSize, callback) {
 console.log('Reading in current circuits...');
 readIn(0, function() {
   console.log('Counting circuits...');
-  var perFlag = {};
-  for (var circuitI=0; circuitI<circuits.length; circuitI++) {
-    var thisCircuit = circuits[circuitI].circuit;
-    var thisStack = circuits[circuitI].stack;
-    for (var wireI=0; wireI<thisStack.length; wireI++) {
-      if (typeof perFlag[thisStack[wireI]] === 'undefined') {
-        perFlag[thisStack[wireI]] = thisCircuit.slice(0, wireI - numVars + 1);
-      }
-    }
-  }
-  console.log(perFlag);
-  for (var flag = 1; flag<=numFlags; flag++) {
-    if (typeof perFlag[flag] === 'undefined') {
-      console.log('Missing', flag);
-    }
-  }
+  // for (var flag = 1; flag<=numFlags; flag++) {
+  //   if (typeof perFlag[flag] === 'undefined') {
+  //     console.log('Missing', flag);
+  //   }
+  // }
   console.log(`Number of flags covered: ${Object.keys(perFlag).length} / ${numFlags}`);
   console.log('Done.');
 });
