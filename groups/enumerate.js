0c449c28efdadaffc09ddccae0a7090463018767
Michiel de Jong <michiel@unhosted.org>
2016-08-30 16:34:21 +0700

    Support 4 vars in theory (but lookup table too large)

--- groups/enumerate.js
+++ groups/enumerate.js
@@ -4,10 +4,21 @@ var readline = require('readline');
 var numVars = parseInt(process.argv[2]);
 var numValuations = Math.pow(2, numVars);
 var numFunctions = Math.pow(2, numValuations - 1); // we don't consider functions that start with 1, so for 3 vars, 01111111 is the highest
-// 128 64 32 16 8 4 2 1
-//              * * * * 15 A
-//         *  *     * * 51 B
-//      *     *   *   * 85 C
+//32768 16384 8192 4096 | 2048 1024 512 256 | 128 64 32 16 | 8 4 2 1
+//                      |                   |              |       * 1 A
+//
+//                      |                   |              |     * * 3 A
+//                      |                   |              |   *   * 5 B
+//
+//                      |                   |              | * * * * 15 A
+//                      |                   |         *  * |     * * 51 B
+//                      |                   |      *     * |   *   * 85 C
+//
+//                      |                   |   *  *  *  * | * * * * 255 A
+//                      |    *    *   *   * |              | * * * * (2048+1024+512+256+15)= 3855 B
+//                *   * |             *   * |         *  * |     * * (8192+4096+512+256+32+16+2+1)=13107 C
+//           *        * |         *       * |      *     * |   *   * (16384+4096+1024+256+64+16+4+1)=21845 D
+//
 // circuits are grouped by the stack of wires they provide
 // all wires are signed so that they're zero if all inputs are zero,
 // and represented by the sum of input valuations for which they are one.
@@ -17,6 +28,7 @@ var circuits = [
       1: [1],
       2: [3, 5],
       3: [15, 51, 85],
+      4: [255, 3855, 13107, 21845],
     }[numVars],
     circuit: [],
   }
@@ -68,6 +80,7 @@ function genGates() {
       res.push(leftRes);
     }
     gates.push(res);
+    console.log('Generated lookup table for gate', gateI);
   }
 }
 
