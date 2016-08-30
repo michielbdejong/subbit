eccb3d145d40fa5d9eb6639e3162c25b3b82b06f
Michiel de Jong <michiel@unhosted.org>
2016-08-30 15:50:49 +0700

    enumerated up to 4 gates

---
+++ groups/count.js
@@ -0,0 +1,68 @@
+var fs = require('fs');
+var readline = require('readline');
+
+var numVars = 3;
+var numValuations = Math.pow(2, numVars);  // for 3 vars, 8 - 000-111
+var numFlags = Math.pow(2, numValuations-1); // for 3 vars, 2^7=128 - 00000000 - 01111111
+
+function readIn(callback) {
+  try {
+    var stream = fs.createReadStream('circuits.txt');
+    stream.on('error', function(err) {
+      console.error('Could not open circuits.txt');
+      callback();
+    });
+    var lineReader = readline.createInterface({
+      input: stream
+    });
+
+    var foundUnreadableLine = false;
+    circuitsRead = [];
+    lineReader.on('line', function (line) {
+      var circuit;
+      try {
+        circuit = JSON.parse(line);
+      } catch (e) {
+        foundUnreadableLine = true;
+      }
+      circuitsRead.push(circuit);
+    });
+    lineReader.on('close', function() {
+      if (foundUnreadableLine) {
+        console.error('Found unreadable line in circuits.txt');
+      } else {
+        circuits = circuitsRead;
+      }
+      callback();
+    });
+  } catch(e) {
+    console.error('Could not read circuits.txt');
+    lineReader.on('close', function() {
+      callback();
+    });
+  }
+}
+
+// ...
+console.log('Reading in current circuits...');
+readIn(function() {
+  console.log('Counting circuits...');
+  var perFlag = {};
+  for (var circuitI=0; circuitI<circuits.length; circuitI++) {
+    var thisCircuit = circuits[circuitI].circuit;
+    var thisStack = circuits[circuitI].stack;
+    for (var wireI=0; wireI<thisStack.length; wireI++) {
+      if (typeof perFlag[thisStack[wireI]] === 'undefined') {
+        perFlag[thisStack[wireI]] = thisCircuit.slice(0, wireI - numVars + 1);
+      }
+    }
+  }
+  console.log(perFlag);
+  for (var flag = 0; flag<numFlags; flag++) {
+    if (typeof perFlag[flag] === 'undefined') {
+      console.log('Missing', flag);
+    }
+  }
+  console.log(`Number of flags covered: ${Object.keys(perFlag).length} / ${numFlags}`);
+  console.log('Done.');
+});
